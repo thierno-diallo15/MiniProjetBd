ab4bb3fec4e2381d501506626d6b2e0bf2cc3adf
l3miage-diallo20 <nenanydara11@gmail.com>
2023-03-13 16:57:13 +0100

    initialisation du projet

---
+++ imprimerie/src/test/java/com/miage/imprimerie/ImprimerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.imprimerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprimerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
