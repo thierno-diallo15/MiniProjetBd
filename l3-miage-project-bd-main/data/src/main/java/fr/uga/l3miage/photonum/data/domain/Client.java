9343272a87ab98f2d8b8976db71b0f3c679ce221
Barry Ibrahima <barryibr@f216-03.e-im2ag.ujf-grenoble.fr>
2023-03-16 14:33:56 +0100

    Mapping du class client

--- l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/domain/Client.java
+++ l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/domain/Client.java
@@ -3,25 +3,45 @@ package fr.uga.l3miage.photonum.data.domain;
 import java.util.List;
 
 import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
-
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Client")
 public class Client {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "email",nullable = false)
     private String email;
+
+    @Column(name = "firsName", nullable = false)
     private String firstName;
+
+    @Column
     private String lastName;
+
+    @Column(name = "password" ,nullable= false)
     private String  password;
+
+    @Column
     private List<String> address;
 
-    public Client() {
-    }
+
+    @OneToMany
+    private List<Impression> impressions ;
+
+    @OneToMany
+    private List<Photo> photos;
+
+    @OneToMany
+    private List<Command> commandes;
 
     public Client(Long id, String email, String firstName, String lastName, String password, List<String> address) {
         this.id = id;
