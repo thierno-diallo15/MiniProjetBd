ea117333f1fcb64c2f4b917ce8228b0f1f8ec89e
Barry <ibrahimafello2017@gmail.com>
2023-03-22 17:32:58 +0100

    version complete du repository

---
+++ l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/repo/PageRepository.java
@@ -0,0 +1,35 @@
+package fr.uga.l3miage.photonum.data.repo;
+
+import java.util.List;
+
+import fr.uga.l3miage.photonum.data.domain.Page;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class PageRepository implements CRUDRepository<Long,Page> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Page  save(Page page) {
+        entityManager.persist(page);
+        return page;
+    }
+    @Override
+    public Page get(Long id) {
+        return entityManager.find(Page.class, id);
+    }
+    @Override
+    public void delete(Page page) {
+        entityManager.remove(page);
+    }
+
+
+    @Override
+    public List<Page> all() {
+        return null;
+    }
+
+    
+}
