6421edf3c9a6044550dfb7e7f1226d4b482307b5
l3miage-mesouaks <salaheddin.mesouak@etu.univ-grenoble-alpes.fr>
2023-03-27 15:08:18 +0200

    Debut Controller

---
+++ l3-miage-project-bd-main/app/src/main/java/fr/uga/l3miage/photonum/Commande/CommandeController.java
@@ -0,0 +1,10 @@
+package fr.uga.l3miage.photonum.Commande;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1", produces = "application/json")
+public class CommandeController {
+
+}
